04878d31284cd843de77de455e2829d49def60cd
Opons1 <98196027+Opons1@users.noreply.github.com>
2026-04-21 11:53:43 -0700

    replace minetest with core (items.lua)

--- items.lua
+++ items.lua
@@ -1,10 +1,10 @@
-minetest.register_craftitem("axistools:craft_knife", {
+core.register_craftitem("axistools:craft_knife", {
 	description = minetest.colorize("#5fff00", "Craftsman's Knife\nIs not consumed in crafting recipes"),
 	inventory_image = "axistools_craftknife.png",
 	stack_max=1
 })
 
-minetest.register_craftitem("axistools:craft_hammer", {
+core.register_craftitem("axistools:craft_hammer", {
 	description = minetest.colorize("#5fff00", "Craftsman's Hammer\nIs not consumed in crafting recipes"),
 	inventory_image = "axistools_crafthammer.png",
 	stack_max=1
